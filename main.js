fd180ea21a325dad8b40d02bc2f5b28381755c04
Abduakhat Valiev <abduakhat.valiev@alif.tj>
2025-08-01 15:44:36 +0500

    added files

---
+++ main.js
@@ -0,0 +1,32 @@
+const quotes = [
+    "It's very hard to take yourself too seriously when you look at the world from outer space.",
+    "Good habits, which bring our lower passions and appetites under automatic control, leave our natures free to explore the larger experiences of life.",
+    "I have been truthful all along the way. The truth is more interesting, and if you tell the truth you never have to cover your tracks.",
+    "They used to photograph Shirley Temple through gauze. They should photograph me through linoleum.",
+    "Humor is just another defense against the universe.",
+    "Laughter is an instant vacation.",
+    "Part of the secret of success in life is to eat what you like and let the food fight it out inside.",
+    "When we try to pick out anything by itself, we find it hitched to everything else in the Universe.",
+    "An ounce of loyalty is worth a pound of cleverness.",
+    "Absolute faith corrupts as absolutely as absolute power.",
+]
+
+const usedIndexes = new Set();
+const quoteElement = document.getElementById("quote");
+
+function generateQuote() {
+  if (usedIndexes.size >= quotes.length) {
+    usedIndexes.clear();
+  }
+
+  while (true) {
+    const randomIdx = Math.floor(Math.random() * quotes.length);
+    
+    if (usedIndexes.has(randomIdx)) continue;
+
+    const quote = quotes[randomIdx];
+    quoteElement.innerHTML = quote;
+    usedIndexes.add(randomIdx);
+    break;
+  }
+}
